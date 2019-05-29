dae457f5c28fd33bf5f0d07782f06bde53c6d493
Samantha Dickeson <samlucydickeson@gmail.com>
2019-05-29 14:39:25 +0100

    add cors?

--- src/app.js
+++ src/app.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
-
+const cors = require("cors");
 const controllers = require("./controllers/index");
 
 const app = express();
 
 app.disable("x-powered-by");
 app.set("port", process.env.PORT || 7000);
+
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: false
   })
 );
+app.use(cors());
 app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(controllers);
 
