af188defeea181d88e1475275cb306f01a87f4eb
Samantha Dickeson <samlucydickeson@gmail.com>
2019-05-30 11:37:06 +0100

    remove need for key

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -7,24 +7,21 @@ const data = require("../utils/neoData.json");
 //Import error functions
 const error = require("./error");
 
-router.get("/", (req, res, next) => {
-  console.log("hi");
+router.get("/", (req, res) => {
   res.send("hi");
 });
 
-router.get("/api/", (req, res, next) => {
-  const { search, key } = req.query;
+router.get("/api/", (req, res) => {
+  const { search } = req.query;
   let result = [];
-  if (key !== "sylvia") res.send("requires key");
-  else {
-    if (typeof search === "number") {
-      result = data.filter(pet => pet.id === search);
-    } else {
-      result = data.filter(pet => pet.name === search);
-    }
-    if (result.length === 0) res.send("no data");
-    else res.json(result);
+
+  if (typeof search === "number") {
+    result = data.filter(pet => pet.id === search);
+  } else {
+    result = data.filter(pet => pet.name === search);
   }
+  if (result.length === 0) res.send("no data");
+  else res.json(result);
 });
 
 //Error handling
